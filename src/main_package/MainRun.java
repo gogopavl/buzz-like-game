a3714f6176cce8307a6a65b3e68f264581ef93bb
Bue <Bue@Bue-PC>
2016-11-09 21:12:54 +0200

    added initialize and gameSetup functions

--- src/main_package/MainRun.java
+++ src/main_package/MainRun.java
@@ -5,8 +5,12 @@
  */
 package main_package;
 
+
+import game.Game;
+import game.Player;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 import rounds.CorrectAnswer;
 
 /**
@@ -14,13 +18,47 @@ import rounds.CorrectAnswer;
  * @author gogopavl
  */
 public class MainRun {
-
+    private static Scanner input;
+    private static Game buzzGame;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        input = new Scanner(System.in);
+                
         
     }
     
+    public static void initializeGame(){
+        int numberOfRounds;
+        
+        System.out.print("Όνομα Χρήστη: ");
+        String nameOfPlayer = input.nextLine();
+        Player player = new Player(nameOfPlayer, 0);
+        
+        
+        System.out.print("Δώσε αριθμό γύρων: ");
+        numberOfRounds = input.nextInt();
+        
+        buzzGame = new Game(numberOfRounds , 1);
+        buzzGame.addPlayer(player);
+    }
+    
+    public static void gameSetup(){
+        int roundModulo;
+        for (int i = 0 ; i < buzzGame.getNumberOfRounds() ; ++i ){
+           roundModulo = i % Game.getNUMBER_OF_ROUND_TYPES(); 
+           switch(roundModulo){
+               case 0: 
+                   //
+                   break;
+               case 1: 
+                   //
+                   break;
+               default: 
+                   //
+                   break;
+           }
+        }         
+    }
 }
