acb5a434ae8359cae39e0900ea5a4713afa44c0c
desppapa <desppapa@192.168.1.90>
2017-01-16 21:58:53 +0200

    Added QuickAnswer, Thermometer classes

---
+++ src/rounds_package/Thermometer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rounds_package;
+
+import java.util.ArrayList;
+import questions_package.Question;
+import static sun.security.krb5.Confounder.intValue;
+
+/**
+ *
+ * @author gogopavl
+ */
+public class Thermometer extends Round{
+    
+    public Thermometer(int nQ, ArrayList<Question> rQ) {
+        super(nQ, rQ);
+    }
+    
+    /////////////////////////////////////////////////
+    //METHODS
+    /////////////////////////////////////////////////
+ 
+    
+    @Override
+    public String getRoundType() {
+        return "Thermometer";
+    }
+    
+    @Override
+    public int evaluateAnwser(Question q, int userInput){
+      
+        if(q.checkAnswer(userInput)){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+}
